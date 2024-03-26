47afff04fffd2a54f52cce8a4d70ba6d89ecafc0
Rudy Nurafif <rudynurafif@gmail.com>
2024-03-27 02:23:01 +0700

    creating stripe checkout functionality

--- src/components/CartPayment.tsx
+++ src/components/CartPayment.tsx
@@ -3,6 +3,8 @@ import FormattedPrice from './FormattedPrice';
 import { useDispatch, useSelector } from 'react-redux';
 import { StateProps, StoreProduct } from '../../type';
 import { useEffect, useState } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { useSession } from 'next-auth/react';
 
 const CartPayment = () => {
   const dispatch = useDispatch();
@@ -17,6 +19,34 @@ const CartPayment = () => {
     });
     setTotalAmount(amount);
   }, [cartData]);
+
+  // Stripe Payment
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
+  const { data: session } = useSession();
+
+  const handleCheckout = async () => {
+    const stripe = await stripePromise;
+
+    const response = await fetch('api/checkout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ items: cartData, email: session?.user?.email }),
+    });
+
+    const checkoutSession = await response.json();
+
+    // Redirecting user or customer to Stripe Checkout
+    const result: any = await stripe?.redirectToCheckout({
+      sessionId: checkoutSession.id,
+    });
+
+    if (result.error) {
+      alert(result?.error.message);
+    }
+  };
 
   return (
     <div className='flex flex-col gap-4'>
@@ -38,7 +68,10 @@ const CartPayment = () => {
       </p>
       {userInfo ? (
         <div className='flex flex-col items-center'>
-          <button className='w-full h-10 text-sm font-semibold bg-amazon_blue text-white rounded-lg hover:bg-amazon_yellow hover:text-black duration-300'>
+          <button
+            onClick={handleCheckout}
+            className='w-full h-10 text-sm font-semibold bg-amazon_blue text-white rounded-lg hover:bg-amazon_yellow hover:text-black duration-300'
+          >
             Proceed to Buy
           </button>
         </div>
